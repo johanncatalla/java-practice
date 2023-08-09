cda4a979722513456a31135a340fc79181d7f3fa
johanncatalla <catallajohann@gmail.com>
2023-08-09 15:49:41 +0800

    snek

---
+++ syntax/Loop.java
@@ -0,0 +1,18 @@
+public class Loop {
+    public static int num = 10;
+    public static void main(String[] args) {
+        for (int i = 0; i < num; i++) {
+            for (int j = 0; j < num; j++) {
+            System.out.print(" ".repeat(j));
+            System.out.print("snek");
+            System.out.println("");
+            }
+            for (int k = num; k > 0; k--) {
+                System.out.print(" ".repeat(k));
+                System.out.print("snek");
+                System.out.println("");
+            }
+        }
+        
+    }
+}
