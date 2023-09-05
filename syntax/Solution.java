ae8cfc3cd8ed9d8ca6d0f18fe3628c0c225c4ef7
Johann Sebastian Catalla <101336933+johanncatalla@users.noreply.github.com>
2023-09-05 12:29:13 +0800

    Add files via upload

--- syntax/Solution.java
+++ syntax/Solution.java
@@ -1,14 +1,26 @@
-// Johann Sebastian Catalla
 import javax.swing.JOptionPane;
+
 public class Solution {
     public static void main(String[] args) {
-    	int num = 0, sum = 0;
-    	while (num >= 0) {
-    		num = Integer.parseInt(JOptionPane.showInputDialog("Enter num: "));
-    		if (num >=0){
-    			sum += num;
-    		}
-    	}
-    	JOptionPane.showMessageDialog(null, "Sum is: "+sum);   
-    }	
+        int arr_count, size;
+        String input = "", even = "";
+         
+         // Get array size and element per nested array
+        arr_count = Integer.parseInt(JOptionPane.showInputDialog("Enter number of array elements: "));
+        size = Integer.parseInt(JOptionPane.showInputDialog("Enter number of elements in array: "));
+        int[][] arr = new int[arr_count][size];
+        
+        // Define array and construct message
+        for (int array = 0; array < arr_count; array++) {
+            for (int element = 0; element < size; element++) {
+                arr[array][element] = Integer.parseInt(JOptionPane.showInputDialog(null, "Array index["+array+"]["+element+"]:"));
+                input += "\nArray index["+array+"]["+element+"]: "+arr[array][element]+"";
+                if (arr[array][element] % 2 == 0) {
+                    even += "\nEven index["+array+"]["+element+"]: "+arr[array][element]+"";
+                } 
+            }
+        }
+        // Show message
+        JOptionPane.showMessageDialog(null, "\nInput Numbers:"+input+"\n\nEven Numbers:"+even);
+    }
 }
