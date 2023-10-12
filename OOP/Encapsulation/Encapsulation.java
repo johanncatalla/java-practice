85c691d23199f8606bd953965581b10c58778eda
Johann Sebastian Catalla <101336933+johanncatalla@users.noreply.github.com>
2023-10-13 07:57:34 +0800

    Create Encapsulation.java

---
+++ OOP/Encapsulation/Encapsulation.java
@@ -0,0 +1,17 @@
+public class Encapsulation {
+    private String name;
+    private double grade;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    public void setGrade(double grade) {
+        this.grade = grade;
+    }
+    public String getName() {
+        return name;
+    }
+    public double getGrade() {
+        return grade;
+    }
+}
